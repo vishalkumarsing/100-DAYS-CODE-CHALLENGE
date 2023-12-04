0fa8da5f12daabe0cf82c4ce4fb823749a4dc3b7
vishal kumar singh <106993591+vishalkumarsing@users.noreply.github.com>
2023-12-04 23:33:24 +0530

    Create maximum-subarray.CPP

---
+++ DAY8/maximum-subarray.CPP
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans = INT_MIN, curr = 0;
+        for(int i : nums) {
+            curr += i;
+            ans = max(ans, curr);
+            if(curr < 0) curr = 0;
+        }
+        return ans;
+    }
+};
