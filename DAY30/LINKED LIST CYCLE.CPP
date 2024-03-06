1788faebc99ea6ae43dfe90f262c999c1d890df7
vishal kumar singh <106993591+vishalkumarsing@users.noreply.github.com>
2024-03-06 22:31:01 +0530

    Create LINKED LIST CYCLE.CPP

---
+++ DAY30/LINKED LIST CYCLE.CPP
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        auto walker = head, runner = head;
+        while (runner && runner->next)
+        {
+            walker = walker->next;
+            runner = runner->next->next;
+            if (walker == runner)
+                return true;
+        }
+        return false;
+    }
+};
